14b79ec8591fed69a815ae9e16d0444290f82e41
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-05-31 16:52:25 +0300

    submits 1 2 many

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,31 @@
 class Pet:
-    pass
+    all = []
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    def __init__(self,name,pet_type,owner=None):
+        self.name =name
+        self.owner = owner
+        if  not pet_type in Pet.PET_TYPES:
+            raise Exception("Invalid pet_type ")
+        self.pet_type =pet_type
+        Pet.all.append(self)
+
+        
 
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all]
+    
+    def add_pet(self,pet):
+        if not isinstance(pet ,Pet):
+            raise TypeError('pet is not in Pet')
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key =lambda pet:pet.name) 
+
+
+
+            
